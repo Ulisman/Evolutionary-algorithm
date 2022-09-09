25b0adc24f75b8f007a04e7f46724034cab60573
Ulrik Isdahl <42532774+Ulisman@users.noreply.github.com>
2022-09-09 22:34:46 +0200

    Update main.py

--- polynomial-search/main.py
+++ polynomial-search/main.py
@@ -8,11 +8,5 @@ for x in range(100):
     population1.crossover()
     population1.mutation()
     print(f'Iteration {counter}: Best fitness: {population1.get_best_fitness()}')
-    #if(population1.get_best_fitness() < 0.01):
-     #   break
-    # counter += 1
-    # for indv in population1.get_population():
-    #     print(indv.chromosome)
-    # print()
 
 print(population1.get_best_fitness())
